c39c5535fd31c11ae284dc67c891d32993e1aa49
Yaron <ymamane5@gmail.com>
2019-08-07 20:54:05 +0300

    Update random_lotto.c

--- random_lotto.c
+++ random_lotto.c
@@ -1,32 +1,70 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<math.h>
+#include<time.h>
 
-void print_board(int board[][]);
+void print_board(char board[][]);
+void update_board(char board[][], int arr[]);
 
 #define ROWS 5
 #define COLUMNS 10
+#define SIZE 6
 
 int main()
 {
-int board[10][5] = {0};
+	char board[ROWS][COLUMNS];
+	int random_numbers[SIZE], temp_rand;
 
-print_board(board);
+	/* initialize board */
+	for(i = 0; i < ROWS; i++) 
+		for(j = 0; j < COLUMNS; j++)
+			board[i][j] = '-';
+
+	srand(time(0));
+
+	while(i < SIZE)
+	{
+		temp_rand = rand() % 50;
+		/*
+		check if exist
+			/*continue;
+		else
+			random_numbers[i++] = temp_rand;
+		*/
+	}
+	update_board(board);
+	print_board(board);
 
 }
 
-void print_board(int board[][])
+void update_board(char board[][], int arr[])
 {
-	int i, j;
-
-	for(i = 0; i < ROWS; i++) 
+	int row, col;
+	
+	for(i = 0; i < SIZE; i++)
 	{
-		for(j = 0; j < COLUMNS; j++)
-		{
-			printf("%d ",board[j][i]);
-		}
-	printf("\n");
+		row = arr[i] <= 10 ? arr[i] : arr[i] / 10;
+		col = arr[i] <= 10 ? : arr[i] % 10;
+		board[row][col] = '+';
 	}
 }
 
+void print_board(char board[][COLUMNS])
+{
+	int i, j;
+	
+	printf("  ");
+	for(i = 1; i <= COLUMNS; i++) 
+		printf("%d ",i);
+	
+	printf("\n");
 
+	for(i = 0; i < ROWS; i++) 
+	{
+		printf("%d ", i);
+			for(j = 0; j < COLUMNS; j++)
+			{
+				printf("%c ",board[i][j]);
+			}
+		printf("\n");
+	}
+}
