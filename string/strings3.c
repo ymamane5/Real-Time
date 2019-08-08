4b89e30d90fa7927d9c8a46ce4d381bb94425b11
Yaron <ymamane5@gmail.com>
2019-08-08 18:52:51 +0300

    Add files via upload

---
+++ string/strings3.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<string.h>
+
+char* squeez(char* s1, char* s2);
+
+int main()
+{
+	printf("\n%s\n", squeez("ABBCD", "QB"));
+	
+	return 0;
+}
+
+char* squeez(char* s1, char* s2)  
+{
+	int i, j, first_index, differ, finish = 0;
+	char temp, toDelete;
+	
+	toDelete = s2[0];	
+	for(i = 1; i < strlen(s2); i++)
+	{
+		for(j = 0; j < strlen(s1); j++)
+		{
+			if(s1[j] == s2[i])
+				s1[j] = toDelete;
+		}
+	}
+	i = 0;
+	while(s[i] != '\0')
+	{
+		if(s1[i++] == toDelete)
+		{
+			first_index = i - 1;
+			while(s1[i] == toDelete)
+				i++;
+			
+			differ = i - first_index;		
+			for(j = 0; j < differ; j++)
+			{
+				s1[first_index] = s1[first_index + differ];
+				if(s1[first_index + differ] == '\0') 
+				{
+					i = first_index;
+					break;
+				}
+				
+				first_index++;
+			}
+			s1[first_index] = '\0';
+		}
+	}
+}
