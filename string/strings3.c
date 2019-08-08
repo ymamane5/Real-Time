dc757aec8e470e9cbf2f668a99a1e917c2674cbb
Yaron <ymamane5@gmail.com>
2019-08-08 22:50:33 +0300

    Update strings3.c

--- string/strings3.c
+++ string/strings3.c
@@ -5,47 +5,50 @@ char* squeez(char* s1, char* s2);
 
 int main()
 {
-	printf("\n%s\n", squeez("ABBCD", "QB"));
-	
+	char s1[8] = "ABBCD", s2[8] = "QB";
+	printf("%s\n", squeez(s1, s2));
+
+	getchar();
 	return 0;
 }
 
-char* squeez(char* s1, char* s2)  
+char* squeez(char* s1, char* s2)
 {
 	int i, j, first_index, differ, finish = 0;
 	char temp, toDelete;
-	
-	toDelete = s2[0];	
-	for(i = 1; i < strlen(s2); i++)
+
+	toDelete = s2[0];
+	for (i = 1; i < strlen(s2); i++)
 	{
-		for(j = 0; j < strlen(s1); j++)
+		for (j = 0; j < strlen(s1); j++)
 		{
-			if(s1[j] == s2[i])
+			if (s1[j] == s2[i])
 				s1[j] = toDelete;
 		}
 	}
 	i = 0;
-	while(s1[i] != '\0')
+	while (s1[i] != '\0')
 	{
-		if(s1[i++] == toDelete)
+		if (s1[i++] == toDelete)
 		{
 			first_index = i - 1;
-			while(s1[i] == toDelete)
+			while (s1[i] == toDelete)
 				i++;
-			
-			differ = i - first_index;		
-			for(j = 0; j < differ; j++)
+
+			differ = i - first_index;
+			for (j = 0; j < differ; j++)
 			{
 				s1[first_index] = s1[first_index + differ];
-				if(s1[first_index + differ] == '\0') 
+				if (s1[first_index + differ] == '\0')
 				{
 					i = first_index;
 					break;
 				}
-				
+
 				first_index++;
 			}
 			s1[first_index] = '\0';
 		}
 	}
+	return s1;
 }
