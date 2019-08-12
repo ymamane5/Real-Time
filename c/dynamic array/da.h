b36069dd19eb0abd1395c19d83c5d69978333f9f
Yaron <ymamane5@gmail.com>
2019-08-12 13:43:15 +0300

    Update da.h

--- c/dynamic array/da.h
+++ c/dynamic array/da.h
@@ -1,14 +1,15 @@
 #ifndef DA_H
 #define DA_H
 
-struct array {
+struct DA {
 	int* arr;
 	int capacity;
 	int index;
-}
+};
 
-void create_DA(int capacity);
-void printDA(DA *da);
-int insert(DA *da, int num);
-void destroy(DA *da);
+void create_DA(int);
+void printDA(DA* da);
+int insert(DA* da, int num);
+void destroy(DA* da);
 
+#endif
