75b6ef9f1bf1e1d7eaf75af9fd3272710494bced
Yaron <ymamane5@gmail.com>
2019-08-12 18:53:45 +0300

    Update da.c

--- c/dynamic array/da.c
+++ c/dynamic array/da.c
@@ -1,16 +1,45 @@
 #include<stdio.h>
 #include "da.h"
 
-void create_DA(int capacity)
+DA* create_DA(int capacity)
 {
 	DA *da = malloc(sizeof(DA));
 	da->capacity = capacity;
 	da->index = 0;
 	da->arr = malloc(sizeof(int) * capacity);
+
+	return da;
 }
 
-void printDA(DA *da) { return; }
+void printDA(DA* da) { return; }
 
-int insert(DA *da, int num) { return num; }
+int insert(DA *da, int num) 
+{ 
+	int* temp;
 
-void destroy(DA *da) { return; }
+	if (num == -1)
+		return 0;
+
+	if (da->index >= da->capacity)
+	{
+		da->arr = realloc(da->arr, (da->capacity) * 2);
+		da->capacity *= 2;
+		/*
+		temp = realloc(da->arr, da->capacity * 2);
+		if (temp != NULL)
+		{
+			da->arr = temp;
+			da->capacity *= 2;
+		}
+		else
+			return 0;
+		*/
+	}
+
+	da->arr[da->index] = num;
+	da->index++;
+	return 1;
+
+}
+
+void destroy(DA *da) { free(da); }
