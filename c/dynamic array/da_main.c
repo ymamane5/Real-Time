5039e5e03e26a183a9ab52e3363394e69400d462
Yaron <ymamane5@gmail.com>
2019-08-12 13:45:23 +0300

    Update da_main.c

--- c/dynamic array/da_main.c
+++ c/dynamic array/da_main.c
@@ -1,33 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-void create_DA(int capacity);
+#include "da.h"
 
 int main()
 {
-
+	struct DA da;
 }
-/*
-files:
-da_main.c
-da.c
-da.h
-*/
 
 
 // if first malloc succeed and the other failed
 //in da.c:
-
-void create_DA(int capacity)
-{
-	DA *da = malloc(sizeof(array));
-	da->capacity = capacity;
-	da->index = 0;
-	da->arr = malloc(sizeof(int) * capacity);
-}
-
-void printDA(DA *da){}
-
-int insert(DA *da, int num){}
-
-void destroy(DA *da){}
