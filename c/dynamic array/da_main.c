25853e36eea13ecaeb07b816ab665f4dcfca7b0b
Yaron <ymamane5@gmail.com>
2019-08-12 18:51:43 +0300

    Update da_main.c

--- c/dynamic array/da_main.c
+++ c/dynamic array/da_main.c
@@ -4,9 +4,37 @@
 
 int main()
 {
-	struct DA da;
+	int i, capacity, num;
+	DA* da_1;
+	
+	printf("please enter array size:\n");
+	scanf_s("%d", &capacity);
+
+	da_1 = create_DA(capacity);
+
+	while (capacity <= 0)
+	{
+		printf("please enter positive number\n");
+		scanf_s("%d", &capacity);
+	}
+
+	printf("capacity = %d\nindex = %d", da_1->capacity, da_1->index);
+
+	printf("please enter numbers\n");
+	do {
+		scanf_s("%d", &num);
+		insert(da_1, num);
+	} while (num > 0);
+
+
+	for (i = 0; i < da_1->index; i++)
+		printf("%d ", da_1->arr[i]);
+	
+
+	/*getch();*/
+	free(da_1->arr);
+	destroy(da_1);
 }
 
 
-// if first malloc succeed and the other failed
-//in da.c:
+
