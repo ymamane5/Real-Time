a673bc66408f3fc41d5c340c35f841a61ed9ec5e
Yaron <ymamane5@gmail.com>
2019-08-13 13:41:20 +0300

    Update main.c

--- c/calendar/main.c
+++ c/calendar/main.c
@@ -5,42 +5,65 @@
 int main()
 {
 	float begin, end;
-	int room;
+	int room, selection, exit = 0;
 	Calendar_t* cal;
+	Meeting_t* meeting_p;
 
 	cal = creatAD(3);
 
-	do{
-		printf("insert begin time:\n");
-		scanf("%f", &begin);
-		printf("insert end time:\n");
-		scanf("%f", &end);
-		printf("insert room:\n");
-		scanf("%d", &room);
-		if (insertMeeting(cal, begin, end, room) == 1)
-			printf("added sucssesfully\n");
+	while(exit == 0)
+	{
+
+		printf("select operation:\n1. add meeting\n2. delete meeting\n3. find meeting\n4. delete calendar\n5. exit");
+		scanf("%d",&selection);
+
+		switch(selection)
+		{
+			case 1: {
+				printf("insert begin time:\n");
+				scanf("%f", &begin);
+				printf("insert end time:\n");
+				scanf("%f", &end);
+				printf("insert room:\n");
+				scanf("%d", &room);
+				if (insertMeeting(cal, begin, end, room) == 1)
+					printf("added sucssesfully\n");
+				else
+					printf("insertion failed");
+			}
+				break;
+			case 2: {
+				printf("insert begin time of meeting to delete:\n");
+				scanf("%f", &begin);
+				if(deleteMeeting(cal, begin) == 1)
+					printf("meeting from %f deleted sucssesfully\n", begin);
+				else
+					printf("delete failed");
+			}
+				break;
+			case 3: {
+				printf("insert begin time of existing meeting:\n");
+				scanf("%f", &begin);
+				meeting_p = findMeeting(cal, begin);
+				if(meeting_p != NULL)
+					printf("meeting found:\nfrom %f to %f in room %d\n",meeting_p->begin, meeting_p->end, meeting_p->room_num);
+				else
+					printf("meeting does not exist");
+			}
+				break;
+				case 4: {
+					if(detroyAD(cal) == 1)
+						printf("calendar delted sucssesfully");
+					else
+						printf("delete failed");
+				}
+			case 5: exit = 1;	
+
+		}
+
+		printDay(cal);
+		printf("%d meetings\n", cal->index);
 	}
-	while(begin == -1 || end == -1 || room == -1);
 
-	printDay(cal);
-	/*
-	int i, capacity, num;
-	DA* da_1;
-	printf("please enter numbers\n");
-	do {
-		scanf_s("%d", &num);
-		insert(da_1, num);
-	} while (num > 0);
-	for (i = 0; i < da_1->index; i++)
-		printf("%d ", da_1->arr[i]);
-	
-	getch();
-	free(da_1->arr);
-	destroy(da_1);
-	*/
-	free(cal->day);
-	free(cal);
-
-	
 	return 0;
 }
