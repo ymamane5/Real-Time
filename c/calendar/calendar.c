b88f5c8b3305dc078bd6d9032db861a79c6ef0b9
Yaron <ymamane5@gmail.com>
2019-08-13 09:23:36 +0300

    Update calendar.c

--- c/calendar/calendar.c
+++ c/calendar/calendar.c
@@ -4,48 +4,32 @@
 
 Calendar_t* creatAD()
 {
-	Calendar_t* calendar;
-	calendar = malloc(sizeof(Calendar_t));
-	calendar->year = 0;
-	calendar->month = 1;
-	calendar->day = *(create_DA(3));
+	Calendar_t* calendar = malloc(sizeof(Calendar_t));
+	calendar->capacity = 3;
+	calendar->index = 0;
+	calendar->day = malloc(sizeof(Meeting_t) * 3);
 	if (calendar == NULL)
 		return NULL;
 
 	return calendar;
 }
 
-DA* create_DA(int capacity)
-{
-	DA *da = malloc(sizeof(DA));
-	da->capacity = capacity;
-	da->index = 0;
-	da->arr = (Meeting_t*)malloc(sizeof(Meeting_t) * capacity);
-
-	return da;
-}
-
-void printDay(DA* da)
+void printDay(Calendar_t* cal)
 {
 	int i;
 
-	for (i = 0; i < da->index; i++)
+	for (i = 0; i < cal->index; i++)
 	{
-		printMeeting(&(da->arr[i]));
+		printf("meeting from %f to %f in room %d", cal->day[i].begin, cal->day[i].end, cal->day[i].room_num);
 	}
-}
-
-void printMeeting(Meeting_t* m)
-{
-	printf("meeting from %f to %f in room %d", m->begin, m->end, m->room_num);
 }
 
 int insertMeeting(Calendar_t* cal, float begin, float end, int room)
 {
-	cal->day.arr[cal->day.index].begin = begin;
-	cal->day.arr[cal->day.index].end = end;
-	cal->day.arr[cal->day.index].room_num = room;
-	(cal->day.index)++;
+	cal->day[cal->index].begin = begin;
+	cal->day[cal->index].end = end;
+	cal->day[cal->index].room_num = room;
+	(cal->index)++;
 
 	return 1;
 }
