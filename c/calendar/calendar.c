a9e56c30409d18d09d31d7b0ea0439781a649645
Yaron <ymamane5@gmail.com>
2019-08-13 10:16:29 +0300

    Update calendar.c

--- c/calendar/calendar.c
+++ c/calendar/calendar.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include "calendar.h"
 
 
-Calendar_t* creatAD()
+Calendar_t* creatAD(int size)
 {
 	Calendar_t* calendar = malloc(sizeof(Calendar_t));
-	calendar->capacity = 3;
+	calendar->capacity = size;
 	calendar->index = 0;
-	calendar->day = malloc(sizeof(Meeting_t) * 3);
+	calendar->day = malloc(sizeof(Meeting_t*) * size);
 	if (calendar == NULL)
 		return NULL;
 
@@ -20,15 +21,18 @@ void printDay(Calendar_t* cal)
 
 	for (i = 0; i < cal->index; i++)
 	{
-		printf("meeting from %f to %f in room %d", cal->day[i].begin, cal->day[i].end, cal->day[i].room_num);
+		printf("meeting from %f to %f in room %d\n", cal->day[i]->begin, cal->day[i]->end, cal->day[i]->room_num);
 	}
 }
 
 int insertMeeting(Calendar_t* cal, float begin, float end, int room)
 {
-	cal->day[cal->index].begin = begin;
-	cal->day[cal->index].end = end;
-	cal->day[cal->index].room_num = room;
+	if(cal == NULL || begin < 0 || end < 0 || room < 0)
+		return 0;
+
+	cal->day[cal->index]->begin = begin;
+	cal->day[cal->index]->end = end;
+	cal->day[cal->index]->room_num = room;
 	(cal->index)++;
 
 	return 1;
