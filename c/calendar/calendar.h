0b525fdd91679455ee37d188e399067235c2a1b8
Yaron <ymamane5@gmail.com>
2019-08-13 10:17:10 +0300

    Update calendar.h

--- c/calendar/calendar.h
+++ c/calendar/calendar.h
@@ -1,20 +1,20 @@
 #ifndef CAL
 #define CAL
 
-typedef struct CALENDAR {
-	int capacity;
-	int index;
-	Meeting_t* day;
-}Calendar_t;
-
 typedef struct MEETING {
 	float begin;
 	float end;
 	int room_num;
 }Meeting_t;
 
+typedef struct CALENDAR {
+	int capacity;
+	int index;
+	Meeting_t** day;
+}Calendar_t;
 
-Calendar_t* creatAD();  /* calendar */
+
+Calendar_t* creatAD(int); 
 void printDay(Calendar_t*);
 int insertMeeting(Calendar_t* cal, float begin, float end, int room);
 
