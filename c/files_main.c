e344fcfeb9154de265528e1837388a49ee6342e3
Yaron <ymamane5@gmail.com>
2019-08-14 08:33:09 +0300

    Update files_main.c

--- c/files_main.c
+++ c/files_main.c
@@ -5,7 +5,7 @@
 #define MAX 60 
 #define MAX_WORD 20
 
-typedef struct 
+typedef struct
 {
 	char* str;
 	int count;
@@ -13,7 +13,7 @@ typedef struct
 
 typedef struct DA
 {
-	word_t* arr;
+	word_t** arr;
 	int capacity;
 	int index;
 }DA;
@@ -23,73 +23,94 @@ void removeSpaces(FILE* fp);
 void countletters(FILE* fp);
 void countWords(FILE* fp);
 void insert(DA* da, char* buff);
+DA* createDA(int capacity);
 
 int main()
 {
-	FILE* fp = fopen("lastRows.txt", "r");
+	FILE* fp;
+	fopen_s(&fp, "lastRows.txt", "r");
 
-	if(fp == NULL)
-	{	
+	if (fp == NULL)
+	{
 		printf("cannot open file\n");
 		exit(0);
 	}
-/*
-	lastNLines(fp, 2);
-	removeSpaces(fp);
-	countletters(fp);
-	*/
+	/*
+		lastNLines(fp, 2);
+		removeSpaces(fp);
+		countletters(fp);
+		*/
 	countWords(fp);
 
 	fclose(fp);
+	getch();
 
 	return 0;
 }
 
 void countWords(FILE* fp)
 {
-	DA da;
+	DA* da = createDA(3);
 	char buff[MAX_WORD];
 
-	da.arr = (word_t*)malloc(sizeof(word_t) * 3);
 	fseek(fp, 0, SEEK_SET);
 
-	while(fscanf(fp, "%s", buff) > 0)
+	while (fscanf(fp, "%s", buff) > 0)
 	{
-		insert(&da, buff);
+		insert(da, buff);
 		//printf("%s\n", buff);
 	}
 
 }
 
+
+
 void insert(DA* da, char* buff)
 {
-	printf("strlen = %ld", strlen(buff));
+
 	da->arr[da->index] = (word_t*)malloc(sizeof(word_t));
-	da->arr[da->index].str = malloc(sizeof(char) * strlen(buff));
-	//printf("%s\n", da->arr[da->index].str);
-	strcpy((da->arr[da->index]).str, buff);
-	printf("%s\n", (da->arr[da->index]).str);
+	da->arr[da->index]->str = malloc(sizeof(char) * strlen(buff));
+	
+	strcpy((da->arr[da->index])->str, buff);
+	printf("da[%d] = %s\n",da->index, (da->arr[da->index])->str);
+	da->index++;
+}
+
+DA* createDA(int capacity)
+{
+	DA *da = malloc(sizeof(DA));
+	if (da == NULL)
+		return NULL;
+	da->capacity = capacity;
+	da->index = 0;
+	da->arr = malloc(sizeof(word_t*) * capacity);
+	if (da->arr == NULL)
+	{
+		free(da);
+		return NULL;
+	}
+	return da;
 }
 
 void countletters(FILE* fp)
 {
-	int arr[58] = {0}, i;
+	int arr[58] = { 0 }, i;
 	char buff[MAX], ch;
 
 	fseek(fp, 0, SEEK_SET);
-	while(fgets(buff, MAX, fp) != NULL)
+	while (fgets(buff, MAX, fp) != NULL)
 	{
-		for(i = 0; buff[i] != 0; i++)
+		for (i = 0; buff[i] != 0; i++)
 		{
-			if(buff[i] < 123 && buff[i] > 64)
+			if (buff[i] < 123 && buff[i] > 64)
 				arr[buff[i] - 65]++;
 		}
 	}
 
 	printf("array:\n");
-	for(i = 0; i < 58; i++)
+	for (i = 0; i < 58; i++)
 	{
-		if(arr[i] > 0)
+		if (arr[i] > 0)
 			printf("%c   %d\n", i + 65, arr[i]);
 	}
 
@@ -102,12 +123,12 @@ void removeSpaces(FILE* fp)
 	int i;
 
 	fseek(fp, 0, SEEK_SET);
-	
-	while(fgets(buff, MAX, fp) != NULL)
+
+	while (fgets(buff, MAX, fp) != NULL)
 	{
-		for(i = 0; buff[i] != 0; i++)
+		for (i = 0; buff[i] != 0; i++)
 		{
-			if(buff[i] != ' ')
+			if (buff[i] != ' ')
 				fprintf(new_fp, "%c", buff[i]);
 		}
 	}
@@ -120,19 +141,19 @@ void lastNLines(FILE* fp, int n)
 	int i = 0;
 	long int* arr = (long int*)malloc(sizeof(int) * n);
 	char buf[MAX];
-	
-	while(1)
+
+	while (1)
 	{
 		arr[i % n] = ftell(fp);
 		i++;
-		if(fgets(buf, MAX, fp) == NULL)
-			break;	
+		if (fgets(buf, MAX, fp) == NULL)
+			break;
 	}
 	fseek(fp, 0, SEEK_SET);
 	printf("array:\n%ld\n%ld\n", arr[0], arr[1]);
 
 	printf("last lines:\n");
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		fseek(fp, arr[i], SEEK_SET);
 		fgets(buf, MAX, fp);
