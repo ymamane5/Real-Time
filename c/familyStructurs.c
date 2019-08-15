9aac0f6708be5099eda39588bce94666a377af27
Yaron <ymamane5@gmail.com>
2019-08-15 11:17:49 +0300

    Update familyStructurs.c

--- c/familyStructurs.c
+++ c/familyStructurs.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include<string.h>
 
+typedef int(*familyDuty)(char*);
+
 struct person {
 	char*	 name;
 	int	 age;
+	familyDuty func;
 };
 
-typedef int(*familyDuty)(char*);
+
 
 struct familyTree {
 	int nMembers;
 	struct person p[2];
-
 	struct familyTree* 	next;
-	familyDuty		func[2];
 };
 
 typedef struct person Person_t;
@@ -21,13 +22,14 @@ typedef struct familyTree FamilyTree_t;
 
 int Refua(char* name) { printf("I'm a doctor\n");		return 0; }
 int Handasa(char* name) { printf("I'm an engineer\n");  return 0; }
-FamilyTree_t Halakhmi = { 2, "Ilan", 28, "Gilad", 26, 0, Handasa , Refua};
-FamilyTree_t Buchler = { 2, "Rita", 38, "Brian", 30, 0, Handasa, Refua};
+FamilyTree_t Halakhmi = { 2, "Ilan", 28, Handasa, "Gilad", 26, Refua, 0};
+FamilyTree_t Buchler = { 2, "Rita", 38, Handasa, "Brian", 30, Refua, 0};
 
-void DoIt(FamilyTree_t* ftPtr, char* name) {
-	
-	int index = strcmp(name, ftPtr->p->name) == 0 ? 0 : 1;
-	ftPtr->func[index](name);
+void DoIt(FamilyTree_t* ftPtr, char* name) {	
+	int i;
+	for(i = 0; strcmp(name, ftPtr->p[i].name) != 0; i++);
+
+	ftPtr->p[i].func(name);
 }
 
 int main() {
