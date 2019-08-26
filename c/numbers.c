528ed7dae1c7df13283c791206ac7ad3516a3869
Yaron <ymamane5@gmail.com>
2019-08-26 20:10:42 +0300

    Update numbers.c

--- c/numbers.c
+++ c/numbers.c
@@ -10,77 +10,77 @@ int* num_divisors(int num, int* divisors_count);
 
 int main()
 {
-printf("\nresult = %d\n",palindrom(32753));
+	int num;
+
+	do {
+		printf("please enter number\n");
+		scanf("%d", &num);
+		printf("\palindrom check = %d\nis_ascending = %d\n", palindrom(num), is_ascending(num));
+	} while (num != -1);
+
 }
 
 int palindrom(int num)
 {
-int arr[11] = {0}, i = 0, lengh;
-/*arr = (int*)malloc(11*sizeof(int));*/
-/* convert to array */
+	int arr[11] = { 0 }, i = 0, length;
 
-convert_to_array(num, arr);
-lengh = arr[10];
+	convert_to_array(num, arr);
+	length = arr[10];
 
-for(i=0; i<10; i++)
+	/* check palindrom */
+	for (i = 0; i < length / 2; i++)
 	{
-	printf("%d ",arr[i]);
+		if (arr[i] != arr[length - 1 - i])
+			return 0;
 	}
-/* check palindrom */
-for(i=0; i<lengh/2; i++)
-	{
-	if(arr[i] != arr[lengh-1-i])
-		return 0;
-	}
-return 1;
+	return 1;
 }
 
 int* convert_to_array(int num, int* arr)
 {
-int lengh = 0, i = 0;
+	int lengh = 0, i = 0;
 
-while(num > 0) 
-{
-	arr[i++] = num % 10;
-	num /= 10;
-	lengh++;
-}
-arr[10] = lengh;
-return arr;
+	while (num > 0)
+	{
+		arr[i++] = num % 10;
+		num /= 10;
+		lengh++;
+	}
+	arr[10] = lengh;
+	return arr;
 }
 
 int is_ascending(int num)
 {
+	int prev;
+	
+	do {
+		prev = num % 10;
+		num /= 10;
+		if ((num % 10) > prev)
+			return 0;
+	} while (num > 0);
 
-do {
-	prev = num % 10;
-	num /= 10;
-	if((num % 10) > prev)
-		return 0;
-}
-while(num > 0);
-
-return 1;
+	return 1;
 
 }
 
 int* num_divisors(int num, int* divisors_count)
 {
-int i, j = 0, num_sqrt = (int)sqrt((double)num);
-int* arr = malloc(num_sqrt * sizeof(int));
+	int i, j = 0, num_sqrt = (int)sqrt((double)num);
+	int* arr = malloc(num_sqrt * sizeof(int));
 
-for(i = 2; i < num_sqrt; i++)
+	for (i = 2; i < num_sqrt; i++)
 	{
-	if(num % i == 0)
+		if (num % i == 0)
 		{
-		arr[j++] = i;
-		arr[j++] = num / i;
+			arr[j++] = i;
+			arr[j++] = num / i;
 		}
 	}
-if(sqrt((double)num) % 1 == 0) 
-	arr[j++] = sqrt((double)num);
+	if (sqrt((double)num) - (int)sqrt((double)num) == 0)
+		arr[j++] = sqrt((double)num);
 
-*divisors_count = j;
-return arr;
+	*divisors_count = j;
+	return arr;
 }
-
