e3ccdcaed2d6a5aabb370ee23797fa1edcdbc5a7
Yaron <ymamane5@gmail.com>
2019-08-27 14:55:08 +0300

    Update main.c

--- Data structurs/hash table/main.c
+++ Data structurs/hash table/main.c
@@ -3,6 +3,8 @@
 #include<string.h>
 #include "hash.h"
 
+void insert_test(hash* myHash);
+
 int hashFunc(unsigned char* str)
 {
 	unsigned int hash = 0;
@@ -16,26 +18,60 @@ int hashFunc(unsigned char* str)
 
 int copareFunc(void* elem1, void* elem2)
 {
-	char* str1 = *(char**)elem1;
-	char* str2 = *(char**)elem2;
-	
-	return strcmp(str1, str2);
+	return strcmp((char*)elem1, (char*)elem2);
 }
 
 int main()
 {
 	char* str = "abcd";
 	char* str2 = "abcde";
-	hash* myHash = createHash(20, hashFunc, copareFunc);
-	if (myHash != NULL)
-		printf("hash table created\n");
-	else {
+	hash* myHash = createHash(10, hashFunc, copareFunc);
+	if (myHash == NULL)
+	{
 		printf("cannot create hash table\n");
 		exit(0);
 	}
-	insert(myHash, str, "value1");
-	insert(myHash, str2, "value1");
+
+	insert_test(myHash);
+
+	printf("found: %s\n", (char*)hashfind(myHash, "key7"));
 	
+}
+
+void insert_test(hash* myHash)
+{
+	char* str = "abcd";
+	char* str2 = "abcde";
+
+	insert(myHash, str, "value1");
+	insert(myHash, str2, "value2");
+	insert(myHash, "key3", "value3");
+	insert(myHash, "key4", "value4");
+	insert(myHash, "key5", "value5");
+	insert(myHash, "key6", "value6");
+	insert(myHash, "key7", "value7");
+
+
+	//printf("myHash->arr[3]->value = %s\n", (char*)(myHash->arr[3]->value));
+	//printf("myHash->arr[4]->value = %s\n", (char*)(myHash->arr[4]->value));
+	//printf("myHash->arr[4(2)]->value = %s\n", (char*)(myHash->arr[4]->next->value));
+}
+
+int destroyHash(hash* hash)
+{
+	int i;
+	node* tempBucket, currNode, prevNode;
+
+	for(i = 0; i < hash->size; i++)
+	{
+		tempBucket = hash->arr[i];
+		currNode = tempBucket->;
+		prevNode = tempBucket;
+		while(currNode != NULL)
+		{
+
+		}
+	}
 }
 
 
