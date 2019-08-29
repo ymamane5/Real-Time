5edb11818bc809017e848e22cf42e9ae06c866ee
Yaron <ymamane5@gmail.com>
2019-08-29 09:11:46 +0300

    Update hash.h

--- Data structurs/hash table/hash.h
+++ Data structurs/hash table/hash.h
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 
 typedef int(*hashFuction1)(unsigned char*);
-typedef int(*compareFunction)(void*, void*); 
+typedef int(*compareFunction)(void*, void*);
+typedef void(*userFunc)(node* node);
 
 typedef struct node {
 	void* key;
@@ -22,5 +23,7 @@ hash* createHash(int size, hashFuction1 hashFunc, compareFunction compFunc);
 int insert(hash* hash, void* key, void* value);
 void* hashfind(hash* hash, void* key);
 int destroyHash(hash* hash);
+int deleteNode(hash* hash, void* key);
+void hashForEach(hash* hash, userFunc func);
 
 #endif
