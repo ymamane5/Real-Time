4dda8e843587ec23d004ec528bc9a7f270a50a6f
Yaron <ymamane5@gmail.com>
2019-08-31 19:23:36 +0300

    Update main.c

--- c/bits/main.c
+++ c/bits/main.c
@@ -4,11 +4,11 @@
 
 int main()
 {
-	char x;
-	int rotate, input;
+	char x, y;
+	int rotate, input, n, p, w, i, j, value, res;
 
 	do {
-		printf("Choose an action\n1. invert x\n2. rotate x\n3. exit\n");
+		printf("Choose an action\n1. invert x\n2. rotate x\n3. set bits\n4. setbits (integer)\n5. exit\n");
 		scanf("%d", &input);
 		switch (input)
 		{
@@ -31,13 +31,53 @@ int main()
 				printf("x rotated = ");
 				char2bin(*rotateN(x, rotate));
 				break;
+			case 3:
+				printf("Please enter char\n");
+				scanf("\n%c", &x);
+				printf("Enter second char\n");
+				scanf("\n%c", &y);
+				printf("Enter num of bits to replace:\n");
+				scanf("\n%d", &n);
+				printf("Enter start position:\n");
+				scanf("\n%d", &p);
+				printf("x = \n");
+				char2bin(x);
+				printf("y = \n");
+				char2bin(y);
+				if(setbits(&x, p, n, y) == 1){
+					printf("final x = \n");
+					char2bin(x);
+				}
+				else{
+					printf("illegal parameters\n");
+				}	
+				break;
+			case 4:
+				printf("Please enter positive number\n");
+				scanf("\n%d", &w);
+				printf("Enter higher location\n");
+				scanf("\n%d", &i);
+				printf("Enter lower location:\n");
+				scanf("\n%d", &j);
+				printf("Enter bit value (0 or 1):\n");
+				scanf("\n%d", &value);
+				res = setbits2(w, i, j, value);
+				if(res != 0)
+				{
+					printf("number = \n");
+					int2bin(w);				
+					printf("new number = \n");
+					int2bin(res);
+				}
+				else{
+					printf("illegal parameters\n");
+				}	
+				break;
+			default:
+				break;
 		}
 	}
-	while (input < 3 && input > 0);
-	
+	while (input < 5 && input > 0);
+
 	return 0;
 }
-
-
-
-
