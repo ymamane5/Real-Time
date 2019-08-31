4b3ed7b4a5e4d12d2d5bb3663820d64e6070a7f2
Yaron <ymamane5@gmail.com>
2019-08-31 19:23:04 +0300

    Update bits.h

--- c/bits/bits.h
+++ c/bits/bits.h
@@ -4,5 +4,8 @@
 unsigned char* invertChar(unsigned char x);
 void char2bin(unsigned char x);
 unsigned char* rotateN(unsigned char x, int n);
+int setbits(char* x, int p, int n, char y);
+int setbits2(int w, int i, int j, int value);
+void int2bin(unsigned int n);
 
 #endif
