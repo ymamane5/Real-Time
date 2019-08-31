3fbdbc93d776ae4e4e0059dda299c3908fab0df2
Yaron <ymamane5@gmail.com>
2019-08-31 19:22:33 +0300

    Update bits.c

--- c/bits/bits.c
+++ c/bits/bits.c
@@ -45,3 +45,77 @@ void char2bin(unsigned char x)
 	}
 	printf("\n");
 }
+
+int setbits(char* x, int p, int n, char y)
+{
+	unsigned char mask = 0xFF;
+
+	if(x == NULL || p < 1 || p > 8 || n < 0 || n > 8)
+		return 0;
+
+	mask <<= n;	
+	y = y & (~mask);
+
+	while(p > n)
+	{
+		y <<= 1;
+		mask <<= 1;
+		mask++;
+		p--;
+	}
+
+	*x = *x & mask;
+	*x = *x | y;	
+
+	return 1;
+}
+
+int setbits2(int w, int i, int j, int value)
+{
+	unsigned int mask = 0xFFFFFFFF;
+	int k;
+
+	if (i < j || w < 0 || i < 0 || i > 31 || j < 0 || j > 31 || value < 0 || value > 1)
+		return 0;
+
+	mask <<= (i - j + 1);
+
+	for (k = 0; k < j; k++)
+	{
+		mask <<= 1;
+		mask++;
+	}
+
+	if (value == 0)
+		w = w & mask;
+	else if (value == 1)
+		w = w | (~mask);
+	else
+		return 0;
+
+	return w;
+}
+
+void int2bin(unsigned int n)
+{
+	char arr[33];
+	int i;
+
+	for(i = 0; i<32; i++)
+		arr[i] = '0';
+
+	for(i = 31; n > 0; i--, n /= 2)
+	{
+		arr[i] = (n % 2 == 0) ? '0' : '1';
+	}
+
+	for(i = 0; arr[i] == '0'; i++);
+
+	while(i < 32){
+		printf("%c", arr[i]);
+		i++;
+	}
+
+	printf("\n");		
+
+}
