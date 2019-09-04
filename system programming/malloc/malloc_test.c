6def7fa1c3dd9f0777c2be722f599fad9f36819c
Yaron <ymamane5@gmail.com>
2019-09-04 13:31:25 +0300

    Update malloc_test.c

--- system programming/malloc/malloc_test.c
+++ system programming/malloc/malloc_test.c
@@ -7,13 +7,24 @@ int main()
 {
 	void* buffer_arr[BUFF_MAX] = {0};
 	void* buffer = memInit(buffer_arr, BUFF_MAX);
-	printf("buffer address = %p\n", buffer);
+	printf("buffer address = %p\n", buffer - 4);
 
 	void* myBlock1 = memAlloc(buffer, 30);
-	printf("myBlock1 address = %p\nmyBlock1 size = %u\n", myBlock1, *(unsigned int*)(myBlock1-4) & 0x7fffffff);
 	void* myBlock2 = memAlloc(buffer, 70);
+	void* myBlock3 = memAlloc(buffer, 15);
 
-	printf("myBlock2 address = %p\nmyBlock2 size = %u\n", myBlock2, *(unsigned int*)(myBlock2-4) & 0x7fffffff);
-	printf("3rd pos = %d\n", *(unsigned int*)(myBlock2 + 72));
+	printf("*block2 = %u\n", *(unsigned int*)(myBlock2-4));
+	memFree(buffer, myBlock2);
+//	myBlock2 = memAlloc(buffer, 8);
+//	memFree(myBlock2);
+	memFree(buffer, myBlock3);
+
+	printf("myBlock1 address = %p\nmyBlock1 size = %u\n", myBlock1, HEADER_VALUE(myBlock1));
+	printf("myBlock2 address = %p\nmyBlock2 size = %u\n", myBlock2, HEADER_VALUE(myBlock2));
+	printf("myBlock3 address = %p\nmyBlock3 size = %u\n", myBlock3, HEADER_VALUE(myBlock3));
+
+	//printf("%p + %u = %p\n", myBlock2, HEADER_VALUE(myBlock2), myBlock2 + HEADER_VALUE(myBlock2));
+	
+	printf("next pos = %d\n", HEADER_VALUE(myBlock3 + HEADER_VALUE(myBlock3) + 4));
 
 }
