6f9164a1ceb146d4ba46d24944008e79f52017c3
Yaron <ymamane5@gmail.com>
2019-09-04 13:30:22 +0300

    Update malloc.c

--- system programming/malloc/malloc.c
+++ system programming/malloc/malloc.c
@@ -4,13 +4,38 @@
 
 int buff_size;
 
+void* memFree(void* buffer, void* address)
+{
+	void *current, *next;
+	int i = 0;
+	
+	current = address;
+	next = current + HEADER_VALUE(current) + 4;
+
+	printf("***memFree***\n");
+	*(unsigned int*)(current - 4) = SET_FREE(current);
+
+	while(!IS_OCCUPIED(next) && i < 10) // is free
+	{
+		printf("next = %p\n", next);
+		printf("unifing %u\n", HEADER_VALUE(current));
+		printf("HEADER_VALUE(next) = %u\n", HEADER_VALUE(next));
+		*(unsigned int*)(current - 4) += HEADER_VALUE(next) + 4;
+		next = current + HEADER_VALUE(current) + 4;
+		i++;
+	}
+
+	printf("after unify: %u\n", HEADER_VALUE(current));
+
+	return address;
+}
+
 void* memAlloc(void* buffer, unsigned int size)
 {
-	void* head;
+	void *head, *current, *next;
 	int noResult = 0, try_insert = 1;
-	unsigned int *current, *next;
 
-	buffer = buffer - 4;
+	buffer = buffer;
 	head = buffer;
 
 	while(size % 4 != 0) 
@@ -18,24 +43,32 @@ void* memAlloc(void* buffer, unsigned int size)
 
 	while(1)
 	{
-		if(IS_OCCUPIED(buffer))
-			buffer += *current + 4;
+		//printf("go to while\n");
+		while(IS_OCCUPIED(buffer)){
+			//printf("add %u\n", (*(unsigned int*)buffer & 0x7fffffff) + 4);
+			buffer += HEADER_VALUE(buffer) + 4;
+			//printf("buffer = %p\n", buffer);
+			//printf("*buffer = %u\n", *(unsigned int*)buffer);
+		}
 
-		if(((int*)buffer - (int*)head) >= buff_size)
+		//printf("buffer - head = %d\n", (char*)buffer - (char*)head);
+		if(((char*)buffer - (char*)head) >= buff_size)
 			break;		
 
-		current = (unsigned int*)buffer;
-		next = buffer + *current + 4;
+		current = buffer;
+		next = buffer + HEADER_VALUE(current) + 4;
 		try_insert = 1;
 
 		while(try_insert)
 		{
-			if (*current >= size)
+			if (HEADER_VALUE(current) >= size)
 			{
-				*(unsigned int*)(buffer + size + 4) = *(unsigned int*)buffer - size - 4;
-				*(unsigned int*)buffer = size;
-				*(unsigned int*)buffer = SET_OCCUPIED(buffer);
-				return buffer + 4;
+				printf("allocating mem\n");
+				*(unsigned int*)(buffer + size) = HEADER_VALUE(buffer) - size - 4;
+				*(unsigned int*)(buffer - 4)= size;
+				*(unsigned int*)(buffer - 4) = SET_OCCUPIED(buffer);
+				printf("aloocated: block = %p   value = %u\n", buffer, HEADER_VALUE(buffer));
+				return buffer;
 			}
 			else
 			{
@@ -43,14 +76,14 @@ void* memAlloc(void* buffer, unsigned int size)
 				{
 					while(!IS_OCCUPIED(next))
 					{
-						*current += *next + 4;
-						next = buffer + *current + 4;
+						*(unsigned int*)(current - 4) += *(unsigned int*)next + 4;
+						next = buffer + HEADER_VALUE(current) + 4;
 					}
 				}
 				else
 				{
 					try_insert = 0;
-					buffer += *current + 4;
+					buffer += HEADER_VALUE(current) + 4;
 				}
 
 			}
