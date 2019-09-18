060b0fc110e171d6e783b5cc04bb5cfdba60409c
Yaron <ymamane5@gmail.com>
2019-09-18 16:09:05 +0300

    Add files via upload

---
+++ system programming/semaphor_imp.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<semaphore.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main()
+{
+
+}
+
+int mysem_init(sem_t *sem, unsigned int value);
+{
+	
+}
+
+int mysem_wait(sem_t *sem)
+{
+	
+}
