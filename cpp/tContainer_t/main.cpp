89e8823a266a19c6a00f711ed23c0062f5a7dfe4
Yaron <ymamane5@gmail.com>
2019-10-09 19:06:54 +0300

    Add files via upload

--- cpp/tContainer_t/main.cpp
+++ cpp/tContainer_t/main.cpp
@@ -1,16 +1,95 @@
 #include <string>
 #include <vector>
+#include <list>
 #include <iostream>
 #include "tContainer_t.h"
 
+using namespace std;
+
 int main()
 {
-	int i = 5;
-
+	int input, val;
+	int* res = NULL;
 	tContainer_t<int, vector<int*> > cont;
-	cont.insert(&i);
-	cout << cont.isEmpty() << endl;
-	cout << *cont.getFirst() << endl;
+	tContainer_t<int, list<int*> > cont2;
+
+	do {
+		cout << "choose option:\n1. insert elements\n2. check if empty\n3. num of elements\n4. first element" << endl;
+		cout << "5. last element\n6. find element\n7. remove element\n8. remove all elements\n9. delete element" << endl;
+		cout << "10. delete all\n11. print data\n12. operator[]\n13. exit" << endl;
+		cin >> input;
+
+		switch (input)
+		{
+		case 1:
+			cout << "enter value:\n";
+			cin >> val;
+			cont.insert(new int(val));
+			break;
+		case 2:
+			if (cont.isEmpty())
+				cout << "Empty" << endl;
+			else
+				cout << "not empty" << endl;
+			break;
+		case 3:
+			cout << cont.contSize() << endl;
+			break;
+		case 4:
+			cout << *cont.getFirst() << endl;
+			break;
+		case 5:
+			cout << *cont.getLast() << endl;
+			break;
+		case 6:
+			cout << "enter value:\n";
+			cin >> val;
+			res = cont.find(val);
+			if (res == NULL)
+				cout << "value not found" << endl;
+			else
+				cout << "found: " << *res << endl;
+			
+			break;
+		case 7:
+			cout << "enter value:\n";
+			cin >> val;
+			res = cont.removeElement(val);
+			if (res == NULL)
+				cout << "remove failed - cant find element" << endl;
+			else
+				cout << "removed element: " << *res << endl;
+			break;
+		case 8:
+			cont.ramoveAll();
+			break;
+		case 9:
+			cout << "enter value:\n";
+			cin >> val;
+			cont.deleteElem(val);
+			cout << "element deleted" << endl;
+			break;
+		case 10:
+			cont.deleteAll();
+			break;
+		case 11:
+			cont.printData();
+			break;
+		case 12:
+			cout << "enter index:\n";
+			cin >> val;
+			try {
+				cout << "element = " << *cont[val] << endl;
+			}
+			catch (int ex) {
+				cout << "Eror: index out of bound\n";
+			}
+			break;
+		default:
+			break;
+		}
+
+	} while (input < 13);
 
 	return 0;
 }
--- cpp/tContainer_t/tContainer_t.h
+++ cpp/tContainer_t/tContainer_t.h
@@ -1,10 +1,21 @@
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <typeinfo>
 
 #ifndef CONTAINER
 #define CONTAINER
 
 using namespace std;
+
+class GreaterVal {
+public:
+	GreaterVal(int i) : val(i) {}
+	bool operator()(int* param) const { return (*param == val); }
+
+private:
+	int val;
+};
 
 template<typename T, typename DTYPE>
 class tContainer_t {
@@ -13,35 +24,121 @@ public:
 	tContainer_t();
 	~tContainer_t();
 	
-	bool operator<(const tContainer_t&) const;
-	bool operator==(const tContainer_t&) const;
+	bool operator<(const tContainer_t& cont) const { return (this->data_st.size() < cont.data_st.size()); }
+	bool operator==(const tContainer_t& cont) const { return (this->data_st.size() == cont.data_st.size()); }
 	bool isEmpty() const { return data_st.empty(); }
 	size_t contSize() const { return data_st.size(); }
 	void insert(T* elem) { data_st.push_back(elem); }
 	const T* getFirst() const { return data_st.front(); }
 	const T* getLast() const { return data_st.back(); }
-	const T* find(T& elem);
+	typename DTYPE::iterator getEnd() const { return data_st.end(); }
+	T* find(const T& elem);
+	T* removeElement(const T& elem);
+	void ramoveAll();
+	void deleteElem(const T& elem);
+	void deleteAll();
+	T* operator[](int index);
 
+	void printData() const;
 
 private:
-	tContainer_t& operator=(const tContainer_t&);
-	tContainer_t(const tContainer_t& cont);
+	tContainer_t& operator=(const tContainer_t&) {}
+	tContainer_t(const tContainer_t& cont) {}
 
 	DTYPE data_st;
 	typename DTYPE::iterator iter_t;
-
 };
 
 template<typename T, typename DTYPE>
-tContainer_t<T, DTYPE>::tContainer_t() { iter_t = data_st.begin(); }
+tContainer_t<T, DTYPE>::tContainer_t() { }
 
 template<typename T, typename DTYPE>
-tContainer_t<T, DTYPE>::~tContainer_t() {}
-
-template<typename T, typename DTYPE>
-T* tContainer_t<T, DTYPE>::find(T& elem)
+tContainer_t<T, DTYPE>::~tContainer_t() 
 {
+	deleteAll();
+}
 
+template<typename T, typename DTYPE>
+T* tContainer_t<T, DTYPE>::find(const T& elem)
+{
+	
+	iter_t = find_if(data_st.begin(), data_st.end(), GreaterVal(elem));
+	
+	if (iter_t == data_st.end())
+		return nullptr;
+	else
+		return *iter_t;
+}
+
+template<typename T, typename DTYPE>
+T* tContainer_t<T, DTYPE>::removeElement(const T& elem)
+{
+	T* res;
+	
+	res = find(elem);
+	if (iter_t != data_st.end())
+	{
+		data_st.erase(iter_t);
+	}
+	
+	return res;
+}
+
+template<typename T, typename DTYPE>
+void tContainer_t<T, DTYPE>::ramoveAll()
+{
+	data_st.erase(data_st.begin(), data_st.end());
+}
+
+template<typename T, typename DTYPE>
+void tContainer_t<T, DTYPE>::deleteElem(const T& elem)
+{
+	T* res = find(elem);
+
+	if (res != NULL)
+	{
+		removeElement(*res);
+		free(res);
+	}
+}
+
+template<typename T, typename DTYPE>
+void tContainer_t<T, DTYPE>::deleteAll()
+{
+	for (T* i : data_st)
+	{
+		free(i);
+	}
+	ramoveAll();
+}
+
+template<typename T, typename DTYPE>
+T* tContainer_t<T, DTYPE>::operator[](int index)
+{
+	if (index >= data_st.size())
+	{
+		throw 1;
+	}
+	
+	if (typeid(DTYPE) == typeid(vector<int*>))
+	{
+		return data_st[index];
+	}
+	else
+	{
+		iter_t = data_st.begin();
+		for (int i = 0; i < index; i++, iter_t++);
+		return *iter_t;
+	}
+}
+
+template<typename T, typename DTYPE>
+void tContainer_t<T, DTYPE>::printData() const
+{
+	for (T* i : data_st)
+	{
+		cout << *i << endl;
+	}
 }
 
 #endif
